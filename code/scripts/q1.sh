6042f47de2b55746b63970fd3516ee83f1916852
ghaidaa kashgari <gkashgar@uci.edu>
2018-10-26 13:48:04 -0700

    q1

---
+++ code/scripts/q1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash  
+
+module load jje/jjeutils jje/kent ( you can add other modules that we need for the questions) 
+
+# 1: Download genome 
+# 2: Download checksum
+# 3: Check #1 vs #2
+# 4: Caldulate size of genome using faSize 
+   making sure to write to the output/reports/directory 
+# 5: Edit the approporiate README.md files to tell us what you did 
